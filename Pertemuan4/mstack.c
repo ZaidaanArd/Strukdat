82402f228dc6e6478bc2b221dc5a4e7b3ecc6357
ZaidaanArd <zaidaanardiansyah@gmail.com>
2025-09-28 19:25:41 +0700

    Membuat stack

---
+++ Pertemuan4/mstack.c
@@ -0,0 +1,73 @@
+/* Program   : mtstack.c */
+/* Deskripsi : file DRIVER modul stack karakter */
+/* NIM/Nama  : 20460124140200 - Muhammad Zaidaan Ardiyansyah */
+/* Tanggal   : 25 September 2025 */
+/***********************************/
+
+
+#include <stdio.h>
+#include "tstack.h"
+
+static const char *b2s(boolean b) { return b ? "true" : "false"; }
+
+static int countFilled(Tstack T)
+{
+	int i, cnt = 0;
+	for (i = 1; i <= 10; i++)
+		if (T.wadah[i] != '#')
+			cnt++;
+	return cnt;
+}
+
+int main(void)
+{
+	Tstack S;
+	int i;
+
+	createStack(&S);
+	char x[20] = "kasur rusak";
+	printf(isPalindrom(x) ? "kasur rusak is palindrom" : "kasur rusak is not palindrom");
+	printf("\n");
+	pushN(&S, 3);
+	printStack(S);
+
+	// printf("=== Test createStack & isEmpty ===\n");
+	// createStack(&S);
+	// printStack(S);
+	// printf("isEmptyStack = %s\n\n", b2s(isEmptyStack(S)));
+
+	// printf("=== Test push ===\n");
+	// push(&S, 'A');
+	// push(&S, 'B');
+	// push(&S, 'C');
+	// printStack(S);
+	// printf("viewStack: ");
+	// viewStack(S);
+	// printf("isFullStack = %s, terisi = %d\n\n", b2s(isFullStack(S)), countFilled(S));
+
+	// printf("=== Test pop ===\n");
+	// pop(&S, &x);
+	// printf("pop -> %c\n", x);
+	// pop(&S, &x);
+	// printf("pop -> %c\n", x);
+	// pop(&S, &x);
+	// printf("pop -> %c\n", x);
+	// pop(&S, &x);
+	// printf("pop (underflow) -> %c\n", x);
+	// printStack(S);
+	// printf("isEmptyStack = %s\n\n", b2s(isEmptyStack(S)));
+
+	// printf("=== Test overflow (push > capacity) ===\n");
+	// createStack(&S);
+	// for (i = 0; i < 11; i++)
+	// {
+	// 	char c = (char)('0' + (i % 10));
+	// 	push(&S, c);
+	// }
+	// printStack(S);
+	// printf("viewStack: ");
+	// viewStack(S);
+	// printf("isFullStack = %s, terisi = %d\n\n", b2s(isFullStack(S)), countFilled(S));
+
+	return 0;
+}
